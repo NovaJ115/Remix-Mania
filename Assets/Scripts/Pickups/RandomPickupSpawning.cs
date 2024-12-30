ffabd3195a4e6ca7faee13d203ad68c974c3cac3
Nova <novajones115@gmail.com>
2024-12-30 17:33:40 -0600

    v1.0.5

--- Assets/Scripts/Pickups/RandomPickupSpawning.cs
+++ Assets/Scripts/Pickups/RandomPickupSpawning.cs
@@ -5,22 +5,24 @@ using System.Collections.Generic;
 public class RandomPickupSpawning : MonoBehaviour
 {
 
-    public Transform[] spawnPoints;
+    public List<GameObject> spawnPoints;
     public int amountOfPickups;
     
     private int randomNumber;
 
-    public int[] chosenNumbers;
+    public List<int> chosenNumbers;
     
     void Start()
     {
-        chosenNumbers = new int[amountOfPickups];
+        
         for (int i = 0; i < amountOfPickups; i++)
         {
             
-            randomNumber = Random.Range(0, spawnPoints.Length);
-            Debug.Log(randomNumber);
-            chosenNumbers[i] = randomNumber;
+            randomNumber = Random.Range(0, spawnPoints.Count);
+            //Debug.Log(randomNumber);
+            Instantiate(Resources.Load("CoinPickup"), new Vector3(spawnPoints[randomNumber].transform.position.x, spawnPoints[randomNumber].transform.position.y, spawnPoints[randomNumber].transform.position.z), Quaternion.identity);
+            spawnPoints.RemoveAt(randomNumber);
+            //chosenNumbers.Add(randomNumber);
         }
     }
 
